264dc9865c213dbda677ad888cb502e409c25828
Water.Li <lizongbin@hotmail.com>
2021-08-01 15:34:57 +0800

    Update MiniDrawer.tsx

--- src/compontents/MiniDrawer.tsx
+++ src/compontents/MiniDrawer.tsx
@@ -17,13 +17,14 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import { SvgIcon, Tooltip } from '@material-ui/core';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'flex-end',
+  justifyContent: 'center',
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
+  color: theme.palette.primary.main,
   ...theme.mixins.toolbar,
 }));
 
@@ -37,6 +38,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
     '& .MuiDrawer-paper': {
       overflowX: 'hidden',
       width: `calc(${theme.spacing(8)} + 1px)`,
+      //background: '#1a233a',
     }
   }),
 );
@@ -56,18 +58,20 @@ export default function MiniDrawer() {
         }}
       >
         <DrawerHeader>
-          <IconButton >
-            
-          </IconButton>
+          <SvgIcon fontSize = "large" >
+            <path fill="currentColor" d="M22,2C22,2 14.36,1.63 8.34,9.88C3.72,16.21 2,22 2,22L3.94,21C5.38,18.5 6.13,17.47 7.54,16C10.07,16.74 12.71,16.65 15,14C13,13.44 11.4,13.57 9.04,13.81C11.69,12 13.5,11.6 16,12L17,10C15.2,9.66 14,9.63 12.22,10.04C14.19,8.65 15.56,7.87 18,8L19.21,6.07C17.65,5.96 16.71,6.13 14.92,6.57C16.53,5.11 18,4.45 20.14,4.32C20.14,4.32 21.19,2.43 22,2Z" />
+          </SvgIcon>
         </DrawerHeader>
         <List>
           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem 
               button key={text} 
             >
-              <ListItemIcon sx={{pl:0.5}}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
+              <Tooltip title={text} arrow placement="right">
+                <ListItemIcon sx={{ml: 0.5, pt: 0.6, pb: 0.6, minWidth:26}}>
+                  <InboxIcon />
+                </ListItemIcon>
+              </Tooltip>
 
             </ListItem>
           ))}
