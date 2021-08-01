81550f66bca5cc5b4ba7aa48f5d19dbd273ae2ba
Water.Li <lizongbin@hotmail.com>
2021-08-01 20:50:07 +0800

    button lose hover

--- src/compontents/MiniDrawer.tsx
+++ src/compontents/MiniDrawer.tsx
@@ -39,7 +39,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
     overflowX: 'hidden',
     '& .MuiDrawer-paper': {
       overflowX: 'hidden',
-      width: theme.spacing(8),
+      width: theme.spacing(9),
       background: '#283046',
       boxShadow: theme.shadows[2],
       display: 'flex',
@@ -59,7 +59,7 @@ export default function MiniDrawer() {
       primary:{
         main: PRIMARY_COLOR,
       },
-      mode: 'dark',
+      mode: 'light',
     },
   });
 
@@ -87,7 +87,11 @@ export default function MiniDrawer() {
                 <path fill="currentColor" d="M22,2C22,2 14.36,1.63 8.34,9.88C3.72,16.21 2,22 2,22L3.94,21C5.38,18.5 6.13,17.47 7.54,16C10.07,16.74 12.71,16.65 15,14C13,13.44 11.4,13.57 9.04,13.81C11.69,12 13.5,11.6 16,12L17,10C15.2,9.66 14,9.63 12.22,10.04C14.19,8.65 15.56,7.87 18,8L19.21,6.07C17.65,5.96 16.71,6.13 14.92,6.57C16.53,5.11 18,4.45 20.14,4.32C20.14,4.32 21.19,2.43 22,2Z" />
               </SvgIcon>
             </Logo>
-            <List>
+            <List sx={{
+              display:'flex',
+              flexFlow:'column',
+              alignItems:'center'
+            }}>
               {
                 MODULES.map((module, index) => {
                   return (
@@ -95,19 +99,39 @@ export default function MiniDrawer() {
                       button key={module.title} 
                       selected={selectedIndex === index}
                       onClick={(event) => handleListItemClick(event, index)}
+                      sx={{
+                        width:'48px',
+                        minWidth: '48px',
+                        height:'40px',
+                        '&.Mui-selected':{
+                          bgcolor: PRIMARY_COLOR,
+                          '&:hover':{
+                            bgcolor: PRIMARY_COLOR,
+                          },
+                        },
+                        display:'flex',
+                        justifyContent: 'center',
+                        mt:0.5,
+                        mb:0.5,
+                        borderRadius: '5px',
+                        '&:hover':{
+                          color:'#fff',
+                        },
+                      }}
                     >
                       <Tooltip title={module.title} arrow placement="right">
                         <ListItemIcon 
                           sx={{
-                            ml: 0.5, 
-                            pt: 0.6, 
-                            pb: 0.6, 
+                            p: 0,
                             minWidth:26,
                             color: selectedIndex === index ? '#fff' :'#7d8ba0',
                             '&:hover':{
                               color: selectedIndex === index ? '#fff' :'rgba(255,255,255,0.8)',
-                            }
-                          }}>
+                            },
+                            display:'flex',
+                            justifyContent: 'center'
+                          }}
+                        >
                           <Box 
                             dangerouslySetInnerHTML={{__html: module.icon}}
                             sx={{
@@ -134,25 +158,30 @@ export default function MiniDrawer() {
             color: '#7d8ba0',
             pb:1,
           }}>
-            <IconButton aria-label="delete" size="large" color = "inherit" 
-              sx={{
-                '&:hover':{
-                  color:'rgba(255,255,255,0.9)',
-                }
-              }}
-            >
-              <Brightness4Icon />
-            </IconButton>
-
-            <IconButton aria-label="delete" size="large" color = "inherit" 
-              sx={{
-                '&:hover':{
-                  color:'rgba(255,255,255,0.9)',
-                }
-              }}
-            >
-              <SettingsIcon />
-            </IconButton>
+            <Tooltip title={'主题'} arrow placement="right">
+              <IconButton aria-label="theme" size="large" color = "inherit" 
+                sx={{
+                  '&:hover':{
+                    color:'rgba(255,255,255,0.9)',
+                  }
+                }}
+              >
+                <SvgIcon>
+                  <path fill="currentColor" d="M19,3H14V5H19V18L14,12V21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10,18H5L10,12M10,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H10V23H12V1H10V3Z" />
+                </SvgIcon>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={'设置'} arrow placement="right">
+              <IconButton aria-label="delete" size="large" color = "inherit" 
+                sx={{
+                  '&:hover':{
+                    color:'rgba(255,255,255,0.9)',
+                  }
+                }}
+              >
+                <SettingsIcon />
+              </IconButton>
+            </Tooltip>
 
             <IconButton sx={{mt:1}} size="large">
               <Avatar alt="User avatar" src="/static/images/avatar.jpg" sx={{ width: 28, height: 28 }} />
@@ -162,33 +191,8 @@ export default function MiniDrawer() {
 
       </ThemeProvider>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+
+              dd
       </Box>
     </Box>
   );
