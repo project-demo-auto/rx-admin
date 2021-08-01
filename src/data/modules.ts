ed61dcd46d38b3d071021543fa179eb747cc95f2
Water.Li <lizongbin@hotmail.com>
2021-08-01 19:11:45 +0800

    Update modules.ts

--- src/data/modules.ts
+++ src/data/modules.ts
@@ -42,7 +42,7 @@ export const MODULES = [
     title:'更多应用',
     icon:`
       <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-        <path fill="currentColor" d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
+        <path fill="currentColor" d="M13 3V11H21V3H13M3 21H11V13H3V21M3 3V11H11V3H3M13 16H16V13H18V16H21V18H18V21H16V18H13V16Z" />
       </svg>
     `,
     url:'/customer'
